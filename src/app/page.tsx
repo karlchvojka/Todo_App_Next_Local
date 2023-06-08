fb05557cc9eba88c3f3c2b4b7ec9e52a01e68f5e
Karl Chvojka <karl.chvojka@protonmail.com>
2023-06-07 22:01:25 -0400

    IMP: Material UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import TestComp from './TestComp'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TestComp title="test" />
+    <main>
+      <TestComp title="Test" />
     </main>
   )
 }
